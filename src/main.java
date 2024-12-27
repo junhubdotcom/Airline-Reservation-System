09bed4081c462c6e4397faf8c04e75f4430929be
David Hong <hwei625@gmail.com>
2024-12-27 17:07:03 +0800

    Update code checking database connection

--- src/main.java
+++ src/main.java
@@ -1,7 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import DataHandle.constants.CommonConstants;
 import UI.main.Login;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -14,6 +20,16 @@ public class main {
                     break;
                 }
             }
+
+            Connection conn = null;
+            conn = DriverManager.getConnection(CommonConstants.DB_URL, CommonConstants.DB_USERNAME, CommonConstants.DB_PASSWORD);
+
+            if (conn.isValid(2)) { // Timeout of 2 seconds
+                System.out.println("Database connection successful!");
+            } else {
+                System.out.println("Database connection failed!");
+            }
+     
         } catch (ClassNotFoundException ex) {
             java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
